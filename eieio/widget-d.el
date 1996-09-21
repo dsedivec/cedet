72c512e2c88747029568fabdc59ef2587605a5e7
zappo <zappo>
1996-09-21 15:55:25 +0000

    Added navigation list to toplevel shell, and added label list to labes for multi-line labels

--- eieio/widget-d.el
+++ eieio/widget-d.el
@@ -149,6 +149,11 @@ widgets which contain children")
   ((buffer :initarg :buffer
 	   :initform current-buffer
 	   :docstring "Buffer this dialog resides in")
+   (logical-child-list :initform nil
+		       :docstring 
+		       "Maintained by the top-level shell, this slot contains a list of all the children in thier logical order
+for the purpose of tab-stepping across them"
+		       :protection private)
    (handle-io :initform t)
    )
   "Definition for a toplevel shell, which contains all children widget
@@ -168,9 +173,6 @@ for a given buffer.")
 consisting of substrings of left, right, center, top, and bottom.
 top-left is assumed if valid values are not found."
 	      :protection private)
-    (invisible :initarg :invisible
-	       :initform nil
-	       :docstring "t if this widget does should have the invisible property.")
     )
   "Definition for a frame, which can contain several children grouped
 in a labeled box.")
@@ -191,6 +193,11 @@ contain widgets of type widget-radio-button.")
   ((label-value :initarg :label-value
 		:initform nil
 		:docstring "The object to display on ourselves")
+   (label-list :initarg nil
+	       :initform nil
+	       :docstring "The label value is transformed into this list, which is broken
+into substrings around carriage returns."
+	       :protection private)
    (justification :initarg :justification
 		  :initform center
 		  :docstring "how to justify the text.  Valid values are 'left, 'center, 'right")
