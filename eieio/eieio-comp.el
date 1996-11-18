eb5bdef9b172755f8ddc8155e0b0a65b393258ab
zappo <zappo>
1996-11-18 00:24:51 +0000

    Fixed the comments

--- eieio/eieio-comp.el
+++ eieio/eieio-comp.el
@@ -4,15 +4,14 @@
 ;;; Copyright (C) 1995,1996 Eric M. Ludlam
 ;;;
 ;;; Author: <zappo@gnu.ai.mit.edu>
-;;; Version: 0.1
 ;;; RCS: $Id$
-;;; Keywords: OO                                           
-;;;                                                                          
+;;; Keywords: oop, lisp, tools
+;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
 ;;; the Free Software Foundation; either version 2, or (at your option)
 ;;; any later version.
-;;;           
+;;;
 ;;; This program is distributed in the hope that it will be useful,
 ;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
