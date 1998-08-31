9652b965c8f1c07c7403f30dc75393f97dbc1754
zappo <zappo>
1998-08-31 02:01:26 +0000

    Changed version number

--- speedbar/speedbar.el
+++ speedbar/speedbar.el
@@ -3,7 +3,7 @@
 ;;; Copyright (C) 1996, 97, 98 Free Software Foundation
 
 ;; Author: Eric M. Ludlam <zappo@gnu.org>
-;; Version: 0.7.2a
+;; Version: 0.7.2b
 ;; Keywords: file, tags, tools
 ;; X-RCS: $Id$
 
