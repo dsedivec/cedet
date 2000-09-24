de53d102b85dc6df98d2837044fb521fd24ca3dd
zappo <zappo>
2000-09-24 15:16:44 +0000

    Copyright update.

--- ede/autoconf-compat.el
+++ ede/autoconf-compat.el
@@ -12,14 +12,12 @@
 ;; Keywords: languages
 ;; $Revision$
 
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software; you can redistribute it and/or modify
+;; This software is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; This software is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
