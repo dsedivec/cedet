1a687d07b6f0e036b35602dbd01b9deed8298497
zappo <zappo>
2000-10-14 01:32:04 +0000

    Enabled :slotofchoice specifier for custom types.

--- eieio/eieio-custom.el
+++ eieio/eieio-custom.el
@@ -181,34 +181,49 @@ of these.")
 		 (slot-boundp obj (car fields)))
 	;; In this case, this field has a custom type.  Create it's
 	;; children widgets.
-	(setq chil (cons (widget-create-child-and-convert
-			  widget
-			  (eieio-filter-slot-type widget (car fcust))
-			  :tag
-			  (concat
-			   (make-string
-			    (or (widget-get widget :indent) 0)
-			    ? )
-			   ;;"Slot "
-			   (if (car flabel)
-			       (car flabel)
-			     (let ((s (symbol-name
-				       (or
-					(class-slot-initarg
-					 (object-class-fast obj)
-					 (car fields))
-					(car fields)))))
-			       (capitalize
-				(if (string-match "^:" s)
-				    (substring s (match-end 0))
-				  s)))))
-			  :value (slot-value obj (car fields))
-			  :sample-face 'eieio-custom-slot-tag-face
-			  )
-			 chil))
+	(let ((type (eieio-filter-slot-type widget (car fcust)))
+	      (stuff nil))
+	  ;; This next bit is an evil hack to get some EDE functions
+	  ;; working the way I like.
+	  (if (and (listp type)
+		   (setq stuff (member :slotofchoices type)))
+	      (let ((choices (eieio-oref obj (car (cdr stuff))))
+		    (newtype nil))
+		(while (not (eq (car type) :slotofchoices))
+		  (setq newtype (cons (car type) newtype)
+			type (cdr type)))
+		(while choices
+		  (setq newtype (cons (list 'const (car choices))
+				      newtype)
+			choices (cdr choices)))
+		(setq type (nreverse newtype))))
+	  (setq chil (cons (widget-create-child-and-convert
+			    widget type
+			    :tag
+			    (concat
+			     (make-string
+			      (or (widget-get widget :indent) 0)
+			      ? )
+			     ;;"Slot "
+			     (if (car flabel)
+				 (car flabel)
+			       (let ((s (symbol-name
+					 (or
+					  (class-slot-initarg
+					   (object-class-fast obj)
+					   (car fields))
+					  (car fields)))))
+				 (capitalize
+				  (if (string-match "^:" s)
+				      (substring s (match-end 0))
+				    s)))))
+			    :value (slot-value obj (car fields))
+			    :sample-face 'eieio-custom-slot-tag-face
+			    )
+			   chil)))
 	(setq chil (cons (widget-create-child-and-convert
 			  widget 'documentation-string
-			  :format "%t   %v"
+			  :format "%t   %v\n"
 			  :tag (make-string
 				(or (widget-get widget :indent) 0)
 				? )
