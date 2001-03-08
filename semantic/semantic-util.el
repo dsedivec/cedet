a8160ee36144fe50abe7cf136cb740c73c2a1a0c
ponced <ponced>
2001-03-08 18:45:47 +0000

    Fixed an infinite loop bug in `semantic-find-nonterminal-by-overlay-next' and `semantic-find-nonterminal-by-overlay-prev' when respectively the last or the first overlay are encountered.

--- semantic/semantic-util.el
+++ semantic/semantic-util.el
@@ -258,7 +258,7 @@ UNTRUSTED"
     (if buffer (set-buffer buffer))
     (if (not start) (setq start (point)))
     (let ((os start) (ol nil))
-      (while (and os (not ol))
+      (while (and os (< os (point-max)) (not ol))
 	(setq os (semantic-overlay-next-change os))
 	(when os
 	  ;; Get overlays at position
@@ -283,7 +283,7 @@ UNTRUSTED"
     (if buffer (set-buffer buffer))
     (if (not start) (setq start (point)))
     (let ((os start) (ol nil))
-      (while (and os (not ol))
+      (while (and os (> os (point-min)) (not ol))
 	(setq os (semantic-overlay-previous-change os))
 	(when os
 	  ;; Get overlays at position
