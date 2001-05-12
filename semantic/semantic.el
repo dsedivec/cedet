c938b4246893672c2a6511c7b62de6e4a190dae2
zappo <zappo>
2001-05-12 13:32:33 +0000

    (semantic-bovinate-nonterminal): When calculating the results, if there was no match, then the result stream equals the input stream, so remove the non-matching token from the return stream, claiming that nothing matched.  This prevents repetitive parsing. (semantic-flex): If the comment-start-skip is matched, but forward-comment does nothing, then make sure we skip intervening boring characters so we don't loop infinitly.

--- semantic/semantic.el
+++ semantic/semantic.el
@@ -1020,6 +1020,7 @@ so far, to be used in the error recovery stack."
       (semantic-bovinate-nonterminal-check stream nonterminal))
 
   (let ((matchlist (cdr (assq nonterminal table)))
+	(starting-stream stream)
         (nt-loop  t)             ;non-terminal loop condition
         nt-popup                 ;non-nil if return from nt recursion
         nt-stack                 ;non-terminal recursion stack
@@ -1174,7 +1175,12 @@ so far, to be used in the error recovery stack."
                     (setq out nil)))
               ;; Nothin?
               ))
-          (setq result (list s out semantic-overlay-error-recovery-stack))
+	  (setq result
+		(if (eq s starting-stream)
+		    (list (cdr s) nil
+			  semantic-overlay-error-recovery-stack)
+		  (list s out
+			semantic-overlay-error-recovery-stack)))
           (if nt-stack
               ;; pop previous state from the nt-stack
               (let ((state (car nt-stack))
@@ -1710,8 +1716,21 @@ LENGTH tokens."
 	       (if semantic-ignore-comments
 		   ;; If the language doesn't deal with comments,
 		   ;; ignore them here.
-		   (progn (forward-comment 1)
-			  (setq ep (point)))
+		   (let ((comment-start-point (point)))
+		     (forward-comment 1)
+		     (if (eq (point) comment-start-point)
+			 ;; In this case our start-skip string failed
+			 ;; to work properly.  Lets try and move over
+			 ;; whatever white space we matched to begin
+			 ;; with.
+			 (skip-syntax-forward "-.'"
+					      (save-excursion
+						(end-of-line)
+						(point)))
+		       (forward-comment 1))
+		     (if (eq (point) comment-start-point)
+			 (error "Strange comment syntax prevents lexical analysis"))
+		     (setq ep (point)))
 		 ;; Language wants comments, link them together.
 		 (if (eq (car (car ts)) 'comment)
 		     (setcdr (cdr (car ts)) (save-excursion
